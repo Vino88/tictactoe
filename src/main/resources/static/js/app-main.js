4802d2d7718b7422f6f8309e706338ce4e0c55dc
VinoTan88 <vino8959@gmail.com>
2024-02-07 17:31:55 +0700

    add validation if board size < 3 and add toast error message

--- src/main/resources/static/js/app-main.js
+++ src/main/resources/static/js/app-main.js
@@ -8,8 +8,25 @@ $(document).ready(function () {
   }
 
   $("#btn-new-game").click(function (event) {
-    $("#new_game").val("yes");
-    $("#form_mark_tile").submit();
+    var number = $("#board_size").val().trim();
+    var toastBody = $("#toastBody");
+
+    if (number < 3) {
+      function showToast() {
+        var toast = new bootstrap.Toast(document.getElementById('toastMessage'));
+        toast.show();
+      }
+
+      toastBody.text("Number must have at least 3 digits.");
+      showToast(); // Show toast with validation message
+      event.preventDefault();// Prevent form submission
+      console.log('kena validasi : ', number)
+    }else {
+        toastBody.text("");
+        $("#new_game").val("yes");
+        $("#form_mark_tile").submit();
+      console.log('tidak kena validasi : ', number)
+    }
   });
 
   $("#link-user-logout").click(function (event) {
